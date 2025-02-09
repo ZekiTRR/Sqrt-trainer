882a772ba24bb989639ef5b520a799be625f804f
Yoko <68815364+ZekiTRR@users.noreply.github.com>
2025-02-09 11:11:54 +0200

    Code/main

---
+++ sqrt.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+class sqrt{
+
+	public static void main(String[] args){
+/*		for(int x=0;x < 21;x = x+1){
+			System.out.println("sqrt(x)" + x + "=" + x*x);
+		}
+*/	
+
+		Scanner myObj = new Scanner(System.in);
+
+		Random rand = new Random();
+		// int x = rand.nextInt(21);
+		int y;
+		while(true){
+			
+
+			int x = rand.nextInt(21);
+			System.out.println("Enter the sqrt of: " + x*x);
+			int res = myObj.nextInt();
+
+			if(res != x){
+				System.out.println(" ");
+				System.out.println("No,sqrt of " + x*x + " was = " + x);
+}
+
+			
+		}		
+
+	}
+}
